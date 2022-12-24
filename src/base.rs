cb020ab719c44559b742299d1bb738ea9b508118
0xb612f <0xb612f@gmail.com>
2022-12-24 15:31:18 +0900

    update

--- src/base.rs
+++ src/base.rs
@@ -1,16 +1,16 @@
-unsafe fn solve(){
+fn solve(){
 }
 fn main(){
-    let t:i32=read1();
-    for i in 1..t+1{
-        print!("Case #{}: ",i);
-        unsafe{ solve() }
-    }
+	let t:i32=read1();
+	for _i in 1..t+1{
+		//print!("Case #{}: ",i);
+		solve()
+	}
 }
 fn read1<T>()->T where T:FromStr, <T as FromStr>::Err: fmt::Debug{
-    let mut s = String::new();
-    io::stdin().read_line(&mut s).unwrap();
-    s.trim().parse().unwrap()
+	let mut s = String::new();
+	io::stdin().read_line(&mut s).unwrap();
+	s.trim().parse().unwrap()
 }
 use std::fmt;
 use std::io;
