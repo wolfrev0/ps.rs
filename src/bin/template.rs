3b33dad7c5a31759b78f2c1a089d554dfb57ff27
tuxedcat <tuxedcat@gmail.com>
2023-10-08 08:44:26 +0900

    InputIterator: inputi

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,12 +1,11 @@
 use psrs::io::*;
-use psrs::consts::*;
 
 fn solve(){
 }
 
 fn main(){
-	// let t:i32=read1();
-	// for i in 1..t+1
+	let t:i32=input1();
+	for _ in 1..t+1
 	// {print!("Case #{}: ",i);solve()}
 	{solve();}
 }
@@ -14,10 +13,10 @@ fn main(){
 #[macro_export]
 macro_rules! inputt {
 	($($t: ty),+) => ({
+		use std::io;
 		let mut __a = String::new();
 		io::stdin().read_line(&mut __a).unwrap();
 		let mut __it = __a.split_whitespace();
 		($(__it.next().unwrap().parse::<$t>().unwrap(),)+)
 	})
 }
-use std::{io, cmp::{min, max}};
--- src/io.rs
+++ src/io.rs
@@ -4,10 +4,14 @@ pub fn input1<T>()->T where T:FromStr, <T as FromStr>::Err: fmt::Debug{
 	s.trim().parse().unwrap()
 }
 
-pub fn inputv<T>()->Vec<T> where T:FromStr{
-	let mut s = String::new();
-	io::stdin().read_line(&mut s).unwrap();
+pub fn inputv<T>(s:&mut String)->Vec<T> where T:FromStr{
+	io::stdin().read_line(s).unwrap();
 	s.split_whitespace().flat_map(str::parse::<T>).collect()
+}
+
+pub fn inputi<'a,T>(s:&'a mut String)->impl Iterator<Item=T>+'a where T:FromStr+'a{
+	io::stdin().read_line(s).unwrap();
+	s.split_whitespace().flat_map(str::parse::<T>)
 }
 
 use std::fmt;
