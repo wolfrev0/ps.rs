6d3df94174ac7bb3165bcc79a2dd6a20cd924a32
tuxedcat <tuxedcat@gmail.com>
2023-10-11 14:36:20 +0900

    tree upd

--- src/tree.rs
+++ src/tree.rs
@@ -1,13 +1,12 @@
 
 pub struct Tree{
-	pub n:usize,
 	pub g:Vec<Vec<usize>>,
 }
-
 impl Tree{
 	pub fn new(n:usize)->Tree{
-		Tree{n,g:vec![Vec::new();n]}
+		Tree{g:vec![Vec::new();n]}
 	}
+	pub fn size(&self)->usize{ self.g.len() }
 	pub fn add_edge(&mut self, x:usize, y:usize){
 		self.g[x].push(y);
 		self.g[y].push(x);
@@ -15,15 +14,13 @@ impl Tree{
 }
 
 pub struct TreeRooted{
-	pub n:usize,
 	pub root:usize,
 	pub p:Vec<usize>,
 	pub ch:Vec<Vec<usize>>,
 }
-
 impl TreeRooted{
 	pub fn new(tr:Tree, root:usize)->TreeRooted{
-		let mut ret = TreeRooted { n:tr.n, root:root, p:vec![0;tr.n], ch:vec![Vec::new();tr.n] };
+		let mut ret = TreeRooted { root:root, p:vec![0;tr.size()], ch:vec![Vec::new();tr.size()] };
 		ret.init_dfs(&tr, root, root);
 		ret
 	}
