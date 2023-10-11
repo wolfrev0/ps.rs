c358b651d20c2377a9ca82647deaf1e2aef599c0
tuxedcat <tuxedcat@gmail.com>
2023-10-12 04:45:26 +0900

    move inputt

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -8,14 +8,4 @@ fn main(){
 	// {print!("Case #{}: ",i);solve()}
 	{solve();}
 }
-#[macro_export]
-macro_rules! inputt {
-	($($t: ty),+) => ({
-		use std::io;
-		let mut __a = String::new();
-		io::stdin().read_line(&mut __a).unwrap();
-		let mut __it = __a.split_whitespace();
-		($(__it.next().unwrap().parse::<$t>().unwrap(),)+)
-	})
-}
 use psrs::io::Printer;
--- src/io.rs
+++ src/io.rs
@@ -36,6 +36,16 @@ impl Drop for Printer{
 		println!("{}",self.s);
 	}
 }
+#[macro_export]
+macro_rules! inputt {
+	($($t: ty),+) => ({
+		use std::io;
+		let mut __a = String::new();
+		io::stdin().read_line(&mut __a).unwrap();
+		let mut __it = __a.split_whitespace();
+		($(__it.next().unwrap().parse::<$t>().unwrap(),)+)
+	})
+}
 
 use std::fmt;
 use std::io;
