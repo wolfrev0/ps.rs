b6436f18f41aea31b266fd04b133dd42c9877ea5
tuxedcat <tuxedcat@gmail.com>
2023-10-12 04:50:52 +0900

    template update

--- src/bin/f.rs
+++ src/bin/f.rs
@@ -1,11 +1,10 @@
-fn solve(){
-	let mut out=Printer::new(); out.flush();
+fn solve(mut out:Printer){out.flush();
 }
 
 fn main(){
 	// let t:i32=input1();
 	// for _ in 1..t+1
 	// {print!("Case #{}: ",i);solve()}
-	{solve();}
+	{solve(Printer::new());}
 }
 use psrs::io::Printer;
--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,20 +1,10 @@
-fn solve(){
+fn solve(mut out:Printer){out.flush();
 }
 
 fn main(){
 	// let t:i32=input1();
 	// for _ in 1..t+1
 	// {print!("Case #{}: ",i);solve()}
-	{solve();}
+	{solve(Printer::new());}
 }
-
-#[macro_export]
-macro_rules! inputt {
-	($($t: ty),+) => ({
-		use std::io;
-		let mut __a = String::new();
-		io::stdin().read_line(&mut __a).unwrap();
-		let mut __it = __a.split_whitespace();
-		($(__it.next().unwrap().parse::<$t>().unwrap(),)+)
-	})
-}
+use psrs::io::Printer;
