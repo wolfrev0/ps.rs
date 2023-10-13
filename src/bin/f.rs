a03bb1e51b82faeff9fd3c8306bb6a4058800ba8
tuxedcat <tuxedcat@gmail.com>
2023-10-14 05:22:00 +0900

    update template code

--- src/bin/f.rs
+++ src/bin/f.rs
@@ -1,10 +1,11 @@
-fn solve(mut out:Printer){out.flush();
+fn solve(out:&mut Printer){
 }
 
 fn main(){
+	let mut out = Printer::new();
 	// let t:i32=input1();
 	// for _ in 1..t+1
 	// {print!("Case #{}: ",i);solve()}
-	{solve(Printer::new());}
+	{solve(&mut out);}
 }
 use psrs::io::Printer;
