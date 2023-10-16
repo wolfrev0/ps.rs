3b3f4c4309a80a76517e85e2fad48feae5bd0ccf
tuxedcat <tuxedcat@gmail.com>
2023-10-16 17:04:06 +0900

    template update

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,11 +1,10 @@
-fn solve(out:&mut Printer){
+fn solve(io:&mut psrs::io::IO){
 }
 
 fn main(){
-	let mut out = Printer::new();
-	// let t:i32=input1();
-	// for _ in 1..t+1
-	// {print!("Case #{}: ",i);solve()}
-	{solve(&mut out);}
+	let mut io = psrs::io::IO::new();
+	// let t:usize=io.pop();
+	// for i in 1..t+1
+	// {io.push("Case #").push(i).push(": ");solve(&mut io)}
+	{solve(&mut io);}
 }
-use psrs::io::Printer;
