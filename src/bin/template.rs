6aa2313597389ee579a078e03bec2cf0636ba962
tuxedcat <tuxedcat@gmail.com>
2023-10-17 22:42:03 +0900

    template minor update

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -3,8 +3,9 @@ fn solve(io:&mut psrs::io::BulkIO){
 
 fn main(){
 	let mut io = psrs::io::BulkIO::new();
-	// let t:usize=io.pop();
-	// for _i in 1..t+1
-	// {io.push("Case #").push(_i).push(": ").flush();solve(&mut io)}
-	{solve(&mut io);}
+	let t:usize=1;//io.pop();
+	for _i in 1..=t{
+		// io.push("Case #").push(_i).push(": ");
+		solve(&mut io);
+	}
 }
