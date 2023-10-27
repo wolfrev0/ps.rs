b7729325ee1bb2a240a125c708f0a05e36a7ea79
tuxedcat <tuxedcat@gmail.com>
2023-10-28 04:23:26 +0900

    lichao type update

--- src/tree/lichao.rs
+++ src/tree/lichao.rs
@@ -1,12 +1,14 @@
-use crate::{geom::line2::Line2, math::structs::{Zero, One, Field, Inf}};
+use crate::{geom::line2::Line2, math::structs::{Field, Zero, One, Inf}};
 
-trait LichaoType<T>{
-	fn eval(&self, x:T)->T;
+pub trait LichaoFuncType{
+	type F:Field+From<i64>;
+	fn eval(&self, x:Self::F)->Self::F;
 	fn id()->Self;
 }
-impl<T> LichaoType<T> for Line2<T> where T:Field+Inf{
-	fn eval(&self, x:T)->T {self.caly(x)}
-	fn id()->Self{Self{a:T::zero(),b:T::one(),c:T::inf()}}
+impl LichaoFuncType for Line2<i64>{
+	type F=i64;
+	fn eval(&self, x:Self::F)->Self::F {self.caly(x)}
+	fn id()->Self{Self{a:Self::F::zero(),b:Self::F::one(),c:Self::F::inf()}}
 }
 
 struct Node<T>{
@@ -15,8 +17,8 @@ struct Node<T>{
 pub struct Lichao<T, const XS:i64, const XE:i64>{
 	a:Vec<Node<T>>,
 }
-
-impl<T, const XS:i64, const XE:i64> Lichao<T, XS, XE> where T:LichaoType<T>{
+//max lichao
+impl<T:LichaoFuncType, const XS:i64, const XE:i64> Lichao<T, XS, XE>{
 	pub fn new()->Lichao<T,XS,XE>{
 		let mut ret = Lichao{a:Vec::new()};
 		ret.alloc();
@@ -25,5 +27,15 @@ impl<T, const XS:i64, const XE:i64> Lichao<T, XS, XE> where T:LichaoType<T>{
 	fn alloc(&mut self)->usize{
 		self.a.push(Node{v:T::id(), l:0, r:0});
 		self.a.len()-1
+	}
+	pub fn insert(&mut self, v:T){
+
+	}
+	fn insert_impl(&mut self, v:T, id:usize, s:i64, e:i64){
+		if e-s==1{
+			if self.a[id].v.eval(T::F::from(s)) < v.eval(T::F::from(s)){
+				self.a[id].v=v;
+			}
+		}
 	}
 }
