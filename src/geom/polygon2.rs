37c140ffee74de042bb3ed4cfa22d7290aed5ed9
tuxedcat <tuxedcat@gmail.com>
2023-10-27 22:14:02 +0900

    ConvexHull

--- src/geom/polygon2.rs
+++ src/geom/polygon2.rs
@@ -28,17 +28,46 @@ pub enum PointOnPolygon2{
 }
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PolygonConvex2<T>{
-	pub a:Vec<Vec2<T>>,
+	a:Vec<Vec2<T>>,
 }
 impl<T> PolygonConvex2<T>
-where T:Copy+PartialEq+Zero<T>+One<T>
+where T:Copy+PartialEq+Ord+Zero<T>+One<T>
 +Add<Output=T>+Mul<Output=T>+Neg<Output=T>+Sub<Output=T>
 +Div<Output=T>+Rem<Output=T>{
-	pub fn normalized(&self)->PolygonConvex2<T>{
-		//TODO
-		assert!(false);
-		self.clone()
+	pub fn from_vertices(mut a:Vec<Vec2<T>>)->Self{
+		match a.iter().min().cloned(){
+			None => PolygonConvex2{a:Vec::new()},
+			Some(base) => {
+				a.sort_by(|l,r|{
+					let det = ccw(base,*r,*l);
+					// base should be left-bottom or bottom-left.
+					// More precisely, It doesn't matter if base is on convex hull.
+					// If it isn't, for some order a[0]<a[1]<...<a[n-1], a[n-1]<a[0] is also true.
+					// To prevent it, degree of (a[0]-base) to (a[-1]-base) should be less than 180.
+					assert!(
+						base<=*l&&base<=*r ||
+						(base.y,base.x)<=(l.y,l.x)&&(base.y,base.x)<=(r.y,r.x));
+					if det.is_zero(){
+						(*l-base).lensq().cmp(&(*r-base).lensq())
+					}else{
+						det.cmp(&T::zero())
+					}
+				});
+				let mut s = Vec::new();
+				for i in a{
+					while let (Some(y),Some(z))=(s.get(s.len().wrapping_sub(2)),s.get(s.len().wrapping_sub(1))){
+						if ccw(*y,*z,i)>T::zero(){
+							break;
+						}
+						s.pop();
+					}
+					s.push(i);
+				}
+				PolygonConvex2{a:s}
+			}
+		}
 	}
+	pub fn get_hull(&self)->Vec<Vec2<T>>{ self.a.clone() }
 	pub fn contains(v:Vec2<T>)->PointOnPolygon2{
 		assert!(false);
     let _ = v;
