2064c90b1a618eeef7512a9f7148fac4cf924126
Dohyun Kim <wolfrevo1337@gmail.com>
2025-10-19 05:57:06 +0900

    update UF

--- src/tree/uf.rs
+++ src/tree/uf.rs
@@ -1,23 +1,24 @@
-use std::mem::swap;
+use std::{cell::Cell, mem::swap};
 
 pub struct UF {
-	pub p: Vec<usize>,
+	pub p: Vec<Cell<usize>>,
 	pub g: Vec<Vec<usize>>,
 }
 impl UF {
 	pub fn new(n: usize) -> UF {
 		UF {
-			p: (0..n).collect(),
+			p: (0..n).map(|x| Cell::new(x)).collect(),
 			g: (0..n).map(|x| vec![x]).collect(),
 		}
 	}
-	pub fn root(&mut self, x: usize) -> usize {
-		self.p[x] = if self.p[x] == x {
+	pub fn root(&self, x: usize) -> usize {
+		let new_px = if self.p[x].get() == x {
 			x
 		} else {
-			self.root(self.p[x])
+			self.root(self.p[x].get())
 		};
-		self.p[x]
+		self.p[x].set(new_px);
+		new_px
 	}
 	pub fn union(&mut self, mut x: usize, mut y: usize) -> bool {
 		x = self.root(x);
@@ -28,7 +29,7 @@ impl UF {
 			if self.g[x].len() < self.g[y].len() {
 				swap(&mut x, &mut y);
 			}
-			self.p[y] = x;
+			self.p[y].set(x);
 			let mut gy = Vec::new();
 			swap(&mut self.g[y], &mut gy);
 			self.g[x].extend(gy);
@@ -36,6 +37,6 @@ impl UF {
 		}
 	}
 	pub fn gsz(&self, x: usize) -> usize {
-		self.g[x].len()
+		self.g[self.root(x)].len()
 	}
 }
