e5b48fe35be31ea3b28da5484ea9a1cef8a4bbb4
Dohyun Kim <wolfrevo1337@gmail.com>
2025-10-19 07:29:03 +0900

    update tasks

--- tool/exec.sh
+++ tool/exec.sh
@@ -3,9 +3,13 @@ set -e
 fileBasenameNoExtension=$1
 cargoOptions=$2
 
+INPUT_PATH=test/in
+OUTPUT_PATH=test/out #/dev/stdout
 ulimit -s 32768
+export RUST_BACKTRACE=1
+
 if [ "$(uname)" == "Darwin" ]; then # Mac OS X platform
-	time cargo run --bin ${fileBasenameNoExtension} --release $cargoOptions < test/in
+	time cargo run --bin ${fileBasenameNoExtension} --release $cargoOptions <$INPUT_PATH >$OUTPUT_PATH 2> >(awk '/User/||/System/||/Elap/||/Max/' > /dev/stderr)
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # GNU/Linux platform
-	/usr/bin/time -v cargo run --bin ${fileBasenameNoExtension} --release $cargoOptions <test/in 2> >(awk '/User/||/System/||/Elap/||/Max/')
+	/usr/bin/time -v cargo run --bin ${fileBasenameNoExtension} --release $cargoOptions <$INPUT_PATH >$OUTPUT_PATH 2> >(awk '/User/||/System/||/Elap/||/Max/' > /dev/stderr)
 fi
